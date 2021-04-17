20e951428201c9118db60825912503addab69e25
Mentalsupernova <mentalsupernovafox@gmail.com>
2021-04-17 17:50:09 +0300

    123

---
+++ app/src/main/java/com/example/test_test/MyAccessibilityService.java
@@ -0,0 +1,53 @@
+package com.example.test_test;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+
+    private static final String TAG = "MyAccessibilityService";
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.e(TAG, "onAccessibilityEvent: ");
+
+        String packageName = event.getPackageName().toString();
+        PackageManager packageManager = this.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, 0);
+            CharSequence applicationLabel = packageManager.getApplicationLabel(applicationInfo);
+            Log.e(TAG, "onAccessibilityEvent: "+applicationLabel );
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.e(TAG, "On interrupt: something went wrong");
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+
+
+        AccessibilityServiceInfo  info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
+                AccessibilityEvent.TYPE_VIEW_FOCUSED;
+
+        // Set the type of feedback your service will provide.
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+
+
+        info.notificationTimeout = 100;
+
+        this.setServiceInfo(info);
+
+        Log.e(TAG,"OnserverConnected: ");x
+    }
+}
